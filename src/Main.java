5ce2a56af6dca02279c3f0fa9edb0e9732d08ac7
Nadezhda-Frimerman <tomns@mail.ru>
2024-03-28 20:37:03 +0500

    Add Exercise 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Exercise 1");
+        byte boys=2;
+        short toys=213;
+        int littelBirds=431;
+        long seeds=75624l;
+        float bread=2.5f;
+        double cake=23.25;
+        System.out.println("Значение переменной boys с типом byte равно " + boys);
+        System.out.println("Значение переменной toys с типом short равно " + toys);
+        System.out.println("Значение переменной littelBirds с типом int равно " + littelBirds);
+        System.out.println("Значение переменной seeds с типом long равно " + seeds);
+        System.out.println("Значение переменной bread с типом float равно " + bread);
+        System.out.println("Значение переменной cake с типом double равно " + cake);
+
     }
 }
